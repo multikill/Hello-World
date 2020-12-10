7c13dcfe1c7f7051594241f42e712a30f10af52e
kage <kage1>
2020-12-10 20:35:12 +0100

    Add build script

--- build.bat
+++ build.bat
@@ -1,2 +1,9 @@
-C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64
-msbuild "Hello World.sln" /verbosity:minimal
+@echo off
+
+set local="%~dp0"
+
+call vcbuild.bat
+
+cd /D %local%
+
+msbuild /p:Configuration=Release /p:Platform=x64 /verbosity:minimal
