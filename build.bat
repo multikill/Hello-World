630327dd55342f4e4eade979ca15511cccf0de34
kage <kage1>
2020-12-11 17:29:21 +0100

    ultimate build file

--- build.bat
+++ build.bat
@@ -1,12 +1,14 @@
 @echo off
 
 set local="%~dp0"
-set path=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\
+set PATH=%PATH%;%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\
 
 call vcbuild.bat
+rem call vcbuild.bat -arch=x86 -host_arch=x64
 
 msbuild /t:Build /p:Configuration=Release /p:Platform=x86 /verbosity:minimal .
-
 msbuild /t:Build /p:Configuration=Release /p:Platform=x64 /verbosity:minimal .
 
 pause
+
+
